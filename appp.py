eccd680ae49ae479905151d71ff612ad1f9325fd
vidizb <107489313+vidizb@users.noreply.github.com>
2022-10-24 06:04:36 +0700

    Add files via upload

---
+++ appp.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+import av
+import mediapipe as mp
+import streamlit as st
+import paho.mqtt.client as mqtt
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+from cvzone.PoseModule import PoseDetector
+import pyglet.media
+import threading
+import os
+import requests
+    
+
+detector = PoseDetector()
+sound = pyglet.media.load("alarm.wav", streaming=False)
+people = False
+
+
+def sendTelegram():
+    path = 'C://'  # Replace your path directory
+    url = 'https://api.telegram.org/bot'
+    token = "5746323643:AAFtQBs1cIwc4bUWBZVu4u2F4X3vige1TA0"  # Replace Your Token Bot
+    chat_id = "739780150"  # Replace Your Chat ID
+    caption = "People Detected!!! "
+    cv2.imwrite(os.path.join(path, img_name), img)
+    files = {'photo': open(path + img_name, 'rb')}
+    resp = requests.post(url + token + '/sendPhoto?chat_id=' + chat_id + '&caption=' + caption, files=files)
+    print(f'Response Code: {resp.status_code}')
+
+def process(img):
+    img.flags.writeable = False
+    img_count = 0
+    breakcount = 0
+    img = cv2.flip(img, 1)
+    img = detector.findPose(img, draw=False)
+    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False)
+    img_name = f'image_{img_count}.png'
+
+    teleThread = threading.Thread(target=sendTelegram, args=())
+    soundThread = threading.Thread(target=sound.play, args=())
+
+    if bboxInfo:
+        cv2.rectangle(img, (120, 20), (470, 80), (0, 0, 255), cv2.FILLED)
+        cv2.putText(img, "PEOPLE DETECTED!!!", (130, 60),
+                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+        breakcount += 1
+
+        if breakcount >= 30:
+            if people == False:
+                img_count += 1
+                soundThread.start()
+                teleThread.start()
+                people = not people
+    else:
+        breakcount = 0
+        if people:
+            people = not people
+    return img
+
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+class VideoProcessor:
+    def recv(self, frame):
+        image = frame.to_ndarray(format="bgr24")
+
+        image = process(image)
+
+        return av.VideoFrame.from_ndarray(image, format="bgr24")
+
+webrtc_ctx = webrtc_streamer(
+    key="WYH",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
+    media_stream_constraints={"video": True, "audio": False},
+    video_processor_factory=VideoProcessor,
+    async_processing=True,
+)
