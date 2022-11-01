2e14c404ea0791db50512d371838a908d4175963
vidizb <107489313+vidizb@users.noreply.github.com>
2022-11-02 05:56:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,94 +27,28 @@ def recordData(condition):
     file.close()
 
 def process(img):
-    img.flags.writeable = False
-    img = cv2.flip(img, 1)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
-    img, faces = detector.findFaceMesh(img, draw=False)
+    img, bboxs = detector.findFaces(img, draw=False)
 
-    if faces:
-        face = faces[0]
-        eyeLeft = [27, 23, 130, 243]  # up, down, left, right
-        eyeRight = [257, 253, 463, 359]  # up, down, left, right
-        mouth = [11, 16, 57, 287]  # up, down, left, right
-        faceId = [27, 23, 130, 243, 257, 253, 463, 359, 11, 16, 57, 287]
-
-        #calculate eye left distance ratio
-        eyeLeft_ver, _ = detector.findDistance(face[eyeLeft[0]], face[eyeLeft[1]])
-        eyeLeft_hor, _ = detector.findDistance(face[eyeLeft[2]], face[eyeLeft[3]])
-        eyeLeft_ratio = int((eyeLeft_ver/eyeLeft_hor)*100)
-        # calculate eye right distance ratio
-        eyeRight_ver, _ = detector.findDistance(face[eyeRight[0]], face[eyeRight[1]])
-        eyeRight_hor, _ = detector.findDistance(face[eyeRight[2]], face[eyeRight[3]])
-        eyeRight_ratio = int((eyeRight_ver / eyeRight_hor) * 100)
-        # calculate mouth distance ratio
-        mouth_ver, _ = detector.findDistance(face[mouth[0]], face[mouth[1]])
-        mouth_hor, _ = detector.findDistance(face[mouth[2]], face[mouth[3]])
-        mouth_ratio = int((mouth_ver / mouth_hor) * 100)
-
-        #display text on image
-        cv2.rectangle(img, (30,20), (400,150), (0,255,255), cv2.FILLED)
-        cv2.putText(img, f'Eye Left Ratio: {eyeLeft_ratio}', (50, 60),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0), 2)
-        cv2.putText(img, f'Eye Right Ratio: {eyeRight_ratio}', (50, 100),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-        cv2.putText(img, f'Eye Mouth Ratio: {mouth_ratio}', (50, 140),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-
-        cv2.rectangle(img, (30, 200), (350, 300), (255,0,0), cv2.FILLED)
-        cv2.putText(img, f'Sleep Count: {counter_s}', (40, 240),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
-        cv2.putText(img, f'Yawn Count: {counter_y}', (40, 280),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
-
-        #drowsiness detection logic
-        
-        breakcount_s, breakcount_y = 0, 0
-        counter_s, counter_y = 0, 0
-        state_s, state_y = False, False
-
-        #------------------------Eye-----------------------------
-        if eyeLeft_ratio <= 50 and eyeRight_ratio <= 50:
-            breakcount_s += 1
-            if breakcount_s >= 30:
-                alert()
-                if state_s == False:
-                    counter_s += 1
-                    sound.play()
-                    board.digital[pin].write(0)
-                    recordData("Sleep")
-                    state_s = not state_s
-        else:
-            breakcount_s = 0
-            if state_s:
-                board.digital[pin].write(1)
-                state_s = not state_s
-
-        # ------------------------Mouth-----------------------------
-        if mouth_ratio > 60:
-            breakcount_y += 1
-            if breakcount_y >= 30:
-                alert()
-                if state_y == False:
-                    counter_y += 1
-                    sound.play()
-                    board.digital[pin].write(0)
-                    recordData("Yawn")
-                    state_y = not state_y
-        else:
-            breakcount_y = 0
-            if state_y:
-                board.digital[pin].write(1)
-                state_y = not state_y
-
-        # print(f'Sleep Count = {counter_s}')
-        # print(f'Yawn Count = {counter_y}')
+    if bboxs:
+        #get the coordinate
+        fx, fy = bboxs[0]["center"][0], bboxs[0]["center"][1]
+        pos = [fx, fy]
 
 
+        cv2.circle(img, (fx, fy), 80, (0, 0, 255), 2)
+        cv2.putText(img, str(pos), (fx+15, fy-15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2 )
+        cv2.line(img, (0, fy), (ws, fy), (0, 0, 0), 2)  # x line
+        cv2.line(img, (fx, hs), (fx, 0), (0, 0, 0), 2)  # y line
+        cv2.circle(img, (fx, fy), 15, (0, 0, 255), cv2.FILLED)
+        cv2.putText(img, "TARGET LOCKED", (850, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3 )
 
-        for id in faceId:
-            cv2.circle(img,face[id], 5, (0,0,255), cv2.FILLED)
+    else:
+        cv2.putText(img, "NO TARGET", (880, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
+        cv2.circle(img, (640, 360), 80, (0, 0, 255), 2)
+        cv2.circle(img, (640, 360), 15, (0, 0, 255), cv2.FILLED)
+        cv2.line(img, (0, 360), (ws, 360), (0, 0, 0), 2)  # x line
+        cv2.line(img, (640, hs), (640, 0), (0, 0, 0), 2)  # y line
+
     return img
 
 
